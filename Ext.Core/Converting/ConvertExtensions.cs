b840de2d72f8bd9d725ca19b755495ff492a9358
Yegor Sytnyk <yegor.sytnyk@gmail.com>
2013-12-30 11:35:30 +1100

    GlobalConstants with SpecialSymbols and ConvertExtensions

---
+++ Ext.Core/Converting/ConvertExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Ext.Core.Converting
+{
+    public static class ConvertExtensions
+    {
+        public static int ConvertToInt(this string str)
+        {
+            return Convert.ToInt32(str);
+        }
+
+        public static bool ConverToBoolean(this string str)
+        {
+            return Convert.ToBoolean(str);
+        }
+
+        public static decimal ConverToDecimal(this string str)
+        {
+            return Convert.ToDecimal(str);
+        }
+
+        public static double ConvertToDouble(this string str)
+        {
+            return Convert.ToDouble(str);
+        }
+    }
+}
---
+++ Ext.Core/GlobalConstants.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ext.Core
+{
+    public static class GlobalConstants
+    {
+        public static class SpecialSymbols
+        {
+            public const string SingleQuote = "\'";
+            public const string DoubleQuote = "\"";
+            public const string Backslash = "\\";
+            public const string Slash = "/";
+            public const string Tab = "/t";
+            public const string NewLine = "\r\n";
+        }
+    }
+}
