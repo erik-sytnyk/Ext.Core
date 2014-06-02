3885bf091531a4fcc16c7f4723f4ef5fc85d39f9
Yegor Sytnyk <yegor.sytnyk@gmail.com>
2014-06-02 20:53:08 +0300

    ListExtensions were added

---
+++ Ext.Core/Collections/ListExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ext.Core.Collections
+{
+    public static class ListExtensions
+    {
+        public static bool AddIfNotThere<T>(this IList<T> list, T value)
+        {
+            if (list.Contains(value))
+            {
+                return false;
+            }
+            list.Add(value);
+            return true;
+        }
+    }
+}
