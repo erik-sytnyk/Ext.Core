85f9042c4825bec830491739c18214072e7e1d99
Olya <olya.zabelina@gmail.com>
2014-10-15 12:05:02 +0300

    more unit tests

---
+++ Ext.Core.Tests/DictionaryExtensionsTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ext.Core.Collections;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ext.Core.Tests
+{
+    [TestClass]
+    public class DictionaryExtensionsTest
+    {
+        [TestMethod]
+        public void add_if_no_entry_test()
+        {
+            var dictionary = new Dictionary<int, string>();
+
+            dictionary.AddIfNoEntry(1, "First");
+            dictionary.AddIfNoEntry(1, "First item");
+            dictionary.AddIfNoEntry(2, "Second");
+
+            Assert.AreEqual(dictionary[1], "First");
+        }
+    }
+}
